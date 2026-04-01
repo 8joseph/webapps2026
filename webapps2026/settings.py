da8c3be555d8b537735d8dbebee23fd17e51b930
Joseph N <joenew1289@gmail.com>
2026-04-01 15:22:21 +0100

    base url + login/register page setup

--- webapps2026/settings.py
+++ webapps2026/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'webapps2026.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- webapps2026/urls.py
+++ webapps2026/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from register import views as register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('payapp.urls'),name='home'),
+    path('register/', register_view.register, name='register'),
+    path('login/', register_view.login, name='login'),
 ]
