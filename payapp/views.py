456b1d6355565f1f683970acafbfb8a7dcec419d
Joseph N <joenew1289@gmail.com>
2026-04-03 13:53:16 +0100

    send transactions

--- payapp/views.py
+++ payapp/views.py
@@ -1,9 +1,23 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import render, redirect
 from django.views.decorators.cache import never_cache
 
 from payapp.forms import TransactionForm, RequestTransactionFrom
 from payapp.models import Transaction
+from register.models import PayAppUser
+
+
+def get_currency_symbol_helper(c):
+    match c:
+        case 'EUR':
+            return '€'
+        case 'USD':
+            return '$'
+        case 'GBP':
+            return '$'
+    return '?'
 
 
 @never_cache    #dont cache so user cannot logout and return to logged in pag
@@ -13,7 +27,33 @@ def home(request):
 @login_required(login_url='login')
 def new_transaction(request):
     form = TransactionForm()
-    return render(request, 'payapp/new-transaction.html', {'form': form})
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            with transaction.atomic():
+                payee_name = form.cleaned_data['payee']
+                amount = form.cleaned_data['amount']
+                payer = request.user
+                payee = PayAppUser.objects.select_for_update().get(username=payee_name)
+                if payer.balance < amount:
+                    messages.error(request, "Your balance is too low!")
+                    return redirect('new_transaction')
+
+                payer.balance -= amount
+                payer.save()
+
+                payee.balance += amount
+                payee.save()
+
+                new_transaction_entry = form.save(commit=False)
+                new_transaction_entry.payer = payer
+                new_transaction_entry.payee = payee
+                new_transaction_entry.status = 'COMPLETED'
+                new_transaction_entry.save()
+                return redirect('new_transaction')
+
+    symbol = get_currency_symbol_helper(request.user.currency)
+    return render(request, 'payapp/new-transaction.html', {'form': form, 'currency_symbol': symbol})
 
 @login_required(login_url='login')
 def request_transaction(request):
